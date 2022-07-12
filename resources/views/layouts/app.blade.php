580aaf915edbf42df078511977256f1b74a4bfab
Norbert Sourou <souwouinnorbert@gmail.com>
2022-07-12 12:19:36 +0100

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -141,8 +141,7 @@
                         <a class="dropdown-item"
 
                            href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();"
+                           onclick="event.preventDefault();                                                    document.getElementById('logout-form').submit();"
                         >
                             <i class="mdi mdi-logout mr-2 text-primary"></i> Signout </a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
